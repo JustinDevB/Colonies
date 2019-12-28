b47f600673d387c00297e199b994551898276d8f
Justin <jbattles393@gmail.com>
2019-12-27 19:18:05 -0500

    First Commit

---
+++ src/main/java/jjbat_000/TownyX/App.java
@@ -0,0 +1,13 @@
+package jjbat_000.TownyX;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
