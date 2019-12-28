070cff01fc343024c4c9373696ba4c92b1016213
Justin <jbattles393@gmail.com>
2019-12-27 20:21:23 -0500

    Added FileSaveException

---
+++ src/main/java/justinDevB/TownyX/Exceptions/FileSaveException.java
@@ -0,0 +1,13 @@
+package justinDevB.TownyX.Exceptions;
+
+public class FileSaveException extends Exception  {
+	
+	public FileSaveException() {
+		super("");
+	}
+	
+	public FileSaveException(String message) {
+		super(message);
+	}
+
+}
