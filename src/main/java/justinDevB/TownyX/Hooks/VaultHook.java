8dd184c836fe507600cefd350133b84dd22d08a8
Justin <jbattles393@gmail.com>
2020-01-20 02:30:59 -0500

    Added in a crap ton of debug messages for economy

--- src/main/java/justinDevB/TownyX/Hooks/VaultHook.java
+++ src/main/java/justinDevB/TownyX/Hooks/VaultHook.java
@@ -1,10 +1,13 @@
 package justinDevB.TownyX.Hooks;
 
+import java.util.logging.Level;
+
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
 import justinDevB.TownyX.TownyX;
+import justinDevB.TownyX.TownyX.Mode;
 import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
@@ -12,13 +15,18 @@ import net.milkbowl.vault.permission.Permission;
 
 public class VaultHook {
 
-	private final TownyX townyx;
+	private static TownyX townyx;
 	private static Economy econ = null;
 	private static Permission perms = null;
 	private static Chat chat = null;
+	private static boolean isDebug = false;
 
 	public VaultHook(TownyX tX) {
-		this.townyx = tX;
+		townyx = tX;
+		if (townyx.getMode() == Mode.DEBUG)
+			isDebug = true;
+		if (isDebug)
+			townyx.getLogger().log(Level.INFO, "Initializing Vault Hook...");
 
 		setupEconomy();
 		setupChat();
@@ -26,24 +34,38 @@ public class VaultHook {
 	}
 
 	private boolean setupEconomy() {
+		if (isDebug)
+			townyx.getLogger().log(Level.INFO, "Initializing Economy...");
 		RegisteredServiceProvider<Economy> rsp = townyx.getServer().getServicesManager().getRegistration(Economy.class);
 		if (rsp == null) {
+			if (isDebug)
+				townyx.getLogger().log(Level.SEVERE, "Economy initialization failed!");
 			return false;
 		}
 		econ = rsp.getProvider();
+		if (isDebug)
+			townyx.getLogger().log(Level.INFO, "Economy initialized!");
 		return econ != null;
 	}
 
 	private boolean setupChat() {
+		if (isDebug)
+			townyx.getLogger().log(Level.INFO, "Initializing Chat...");
 		RegisteredServiceProvider<Chat> rsp = townyx.getServer().getServicesManager().getRegistration(Chat.class);
 		chat = rsp.getProvider();
+		if (isDebug)
+			townyx.getLogger().log(Level.INFO, "Chat initialized!");
 		return chat != null;
 	}
 
 	private boolean setupPermissions() {
+		if (isDebug)
+			townyx.getLogger().log(Level.INFO, "Initializing Permissions...");
 		RegisteredServiceProvider<Permission> rsp = townyx.getServer().getServicesManager()
 				.getRegistration(Permission.class);
 		perms = rsp.getProvider();
+		if (isDebug)
+			townyx.getLogger().log(Level.INFO, "Permissions initialized!");
 		return perms != null;
 	}
 
@@ -55,7 +77,15 @@ public class VaultHook {
 	 * @return whether or not it was successful
 	 */
 	public static EconomyResponse depositAmount(OfflinePlayer player, double amount) {
+		if (isDebug)
+			townyx.getLogger().log(Level.INFO, "Attempting to deposit " + amount + " to player " + player.getName());
 		EconomyResponse r = econ.depositPlayer(player, amount);
+		if (isDebug) {
+			if (r.transactionSuccess())
+				townyx.getLogger().log(Level.INFO, "Successfully deposited " + amount + " from " + player.getName());
+			else
+				townyx.getLogger().log(Level.SEVERE, "Could not deposit " + amount + " to player " + player.getName());
+		}
 		return r;
 	}
 
@@ -66,6 +96,9 @@ public class VaultHook {
 	 * @return balance
 	 */
 	public static double getBalance(OfflinePlayer player) {
+		if (isDebug)
+			townyx.getLogger().log(Level.INFO,
+					"Balance of player " + player.getName() + " is " + econ.getBalance(player));
 		return econ.getBalance(player);
 	}
 
@@ -77,7 +110,16 @@ public class VaultHook {
 	 * @return
 	 */
 	public static EconomyResponse subtract(OfflinePlayer player, double amount) {
+		if (isDebug)
+			townyx.getLogger().log(Level.INFO, "Attempting to subtract " + amount + " from player " + player.getName());
 		EconomyResponse r = econ.withdrawPlayer(player, amount);
+		if (isDebug) {
+			if (r.transactionSuccess())
+				townyx.getLogger().log(Level.INFO, "Successfully subtracted " + amount + " from " + player.getName());
+			else
+				townyx.getLogger().log(Level.SEVERE,
+						"Could not subtract " + amount + " from player " + player.getName());
+		}
 		return r;
 	}
 
@@ -89,18 +131,29 @@ public class VaultHook {
 	 * @return
 	 */
 	public static EconomyResponse set(OfflinePlayer player, double amount) {
+		if (isDebug)
+			townyx.getLogger().log(Level.INFO, "Attempting to set " + player.getName() + "'s balance to " + amount);
 		econ.withdrawPlayer(player, getBalance(player));
 		EconomyResponse r = econ.depositPlayer(player, amount);
+		if (isDebug) {
+			if (r.transactionSuccess())
+				townyx.getLogger().log(Level.INFO, "Successfully set " + player.getName() + "'s balance to " + amount);
+			else
+				townyx.getLogger().log(Level.SEVERE, "Could not set " + player.getName() + "'s balance to " + amount);
+		}
 		return r;
 	}
 
 	/**
 	 * Check to see if player has enough for transaction
+	 * 
 	 * @param player to check
 	 * @param amount to check against
 	 * @return true if enough, false if not
 	 */
 	public static boolean hasEnough(OfflinePlayer player, double amount) {
+		if (isDebug)
+			townyx.getLogger().log(Level.INFO, "Checking if " + player.getName() + " can afford " + amount);
 		if (getBalance(player) >= amount)
 			return true;
 		else
